7b7dbd17202e900bea0cf06cb915193a2f05396b
Clay <wcl58981298@gmail.com>
2019-08-12 10:24:47 +0800

    init vue-template

---
+++ template/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routes from './routes';<% if (routerHistoryMode) { %>
+import history from './history';
+
+const router = new VueRouter(history.transformBefore({
+  routes,
+  mode: 'history',
+  // 外链访问的路径，publicPath 在 webpack 中配置
+  // eslint-disable-next-line
+  base: publicPath,
+}));<% } else { %>
+const router = new VueRouter({
+  routes,
+});<% } %>
+
+Vue.use(VueRouter);
+
+router.beforeEach((to, form, next) => {
+  if (to.meta && to.meta.title) document.title = to.meta.title;
+  return next();
+});
+
+export default router;
